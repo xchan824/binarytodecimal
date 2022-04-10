854d9aaa2faa959e7cc0249d0e414c03942aaed9
Christian Paulo Gomonit <xchan824@gmail.com>
2022-04-11 00:39:39 +0800

    modified index.html, main.js, and style.css

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const result = document.getElementById("result");
 
 function binToDec() {
-    const binNum = document.getElementById("binInput").value;
-    let decimalNum = 0;
-    
-    let binNumLength = binNum.length;
+  const binNum = document.getElementById("binInput").value;
+  let decimalNum = 0;
+  
+  let binNumLength = binNum.length;
 
-    for (const digit of binNum) {
+  for (const digit of binNum) {
+    if (!(digit == 1 || digit == 0)) {
+      result.textContent = "Please enter only 1s and 0s.";
+      break;
+    } else {
       decimalNum += digit * (2 ** (binNumLength - 1));
       binNumLength--;
+      result.textContent = decimalNum;
     }
-
-    result.textContent = decimalNum;
+  }
 }
